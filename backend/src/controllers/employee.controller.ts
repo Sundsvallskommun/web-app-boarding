5683351a25d1df6845a83cc37aa9eba958f5a721
Henrik Sandström <henrik.sandstrom@sundsvall.se>
2024-11-21 10:22:17 +0100

    Add user to request object

--- backend/src/controllers/employee.controller.ts
+++ backend/src/controllers/employee.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Param, UseBefore } from 'routing-controllers';
+import { Controller, Get, Param, Req, UseBefore } from 'routing-controllers';
 import ApiService from '@services/api.service';
 import { OpenAPI, ResponseSchema } from 'routing-controllers-openapi';
 import authMiddleware from '@middlewares/auth.middleware';
 import { HttpException } from '@exceptions/HttpException';
 import { PortalPersonData } from '@/data-contracts/employee/data-contracts';
 import { EmployeeApiResponse } from '@/responses/employee.response';
+import { RequestWithUser } from '@/interfaces/auth.interface';
 
 interface ResponseData<T> {
   data: T;
@@ -20,12 +21,12 @@ export class EmployeeController {
   @OpenAPI({ summary: 'Fetch employee' })
   @ResponseSchema(EmployeeApiResponse)
   @UseBefore(authMiddleware)
-  async getChecklistsAsManager(@Param('username') username: string): Promise<ResponseData<PortalPersonData>> {
-    if (!username) {
+  async getChecklistsAsManager(@Req() req: RequestWithUser, @Param('username') username: string): Promise<ResponseData<PortalPersonData>> {
+    if (!req.user.username) {
       throw new HttpException(400, 'Bad request');
     }
     const url = `employee/1.0/portalpersondata/PERSONAL/${username}`;
-    const res = await this.apiService.get<PortalPersonData>({ url });
+    const res = await this.apiService.get<PortalPersonData>({ url }, req.user);
     return { data: res.data, status: res.status, message: 'success' };
   }
 }
