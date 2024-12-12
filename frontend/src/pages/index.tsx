480df13d27b1f178397df935a75a787ef4dfddb2
Jerker Osatie <jerker.osatie@devize.se>
2024-12-12 15:51:04 +0100

    rebased

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -36,84 +36,88 @@ export function Index() {
   };
 
   return (
-    delegatedChecklistsLoaded && (
-      <DefaultLayout title={`${process.env.NEXT_PUBLIC_APP_NAME}`}>
-        <Main>
-          <div className="py-10 px-10 2xl:px-0">
-            {checklist && checklistLoaded ?
-              <div className="flex justify-between border-1 border-divider rounded-button bg-background-content px-16 pt-16 pb-16 mb-40 w-1/3">
-                <div className="flex">
-                  <Avatar
-                    rounded
-                    initials={`${checklist.employee.firstName[0]}${checklist.employee.lastName[0]}`}
-                    size="md"
-                  />
-                  <div className="px-16">
-                    <strong>
-                      {checklist.employee.firstName} {checklist.employee.lastName}
-                    </strong>
-                    <p className="text-small my-0">
-                      <Icon name="check" size="1.8rem" className="align-top mr-6" />
-                      {t('task:activities_completed', {
-                        first: countAllCompletedTasks(checklist),
-                        second: countAllTasks(checklist),
-                      })}
-                    </p>
-                  </div>
-                </div>
-
-                <div>
-                  <Button
-                    iconButton
-                    leftIcon={<Icon name="arrow-right" />}
-                    onClick={() => {
-                      router.push(`/${username}`);
-                    }}
-                  />
+    <DefaultLayout title={`${process.env.NEXT_PUBLIC_APP_NAME}`}>
+      <Main>
+        <div className="py-10 px-10 2xl:px-0">
+          {checklist && checklistLoaded ?
+            <div className="flex justify-between border-1 border-divider rounded-button bg-background-content px-16 pt-16 pb-16 mb-40 w-1/3">
+              <div className="flex">
+                <Avatar
+                  rounded
+                  initials={`${checklist.employee.firstName[0]}${checklist.employee.lastName[0]}`}
+                  size="md"
+                />
+                <div className="px-16">
+                  <strong>
+                    {checklist.employee.firstName} {checklist.employee.lastName}
+                  </strong>
+                  <p className="text-small my-0">
+                    <Icon name="check" size="1.8rem" className="align-top mr-6" />
+                    {t('task:activities_completed', {
+                      first: countAllCompletedTasks(checklist),
+                      second: countAllTasks(checklist),
+                    })}
+                  </p>
                 </div>
               </div>
-            : null}
 
-            {managedChecklists.length ?
-              <>
-                <h2 className="my-16">{capitalize(t('checklists:ongoing_checklists'))}</h2>
-                <p className="mb-16">
-                  {t('checklists:you_got_ongoing_checklists', { count: managedChecklists?.length })}
-                </p>
-
-                <FormProvider {...methods}>
-                  <OngoingChecklistsTable data={managedChecklists} delegatedChecklists={false} />
-                </FormProvider>
-              </>
-            : null}
-
-            {delegatedChecklists.length ?
-              <div className="py-24">
-                <h2 className="mb-16">{t('common:assigned_introductions')}</h2>
-                <p className="mb-16">
-                  {t('checklists:you_got_assigned_introductions', { count: delegatedChecklists?.length })}
-                </p>
-                {delegatedChecklists ?
-                  <OngoingChecklistsTable data={delegatedChecklists} delegatedChecklists={true} />
-                : null}
+              <div>
+                <Button
+                  iconButton
+                  leftIcon={<Icon name="arrow-right" />}
+                  onClick={() => {
+                    router.push(`/${username}`);
+                  }}
+                />
               </div>
-            : null}
-          </div>
+            </div>
+          : null}
 
-          <FormProvider {...methods}>
-            <DelegateMultipleChecklistsModal checklistIds={checked} onClose={closeHandler} isOpen={isOpen} />
-          </FormProvider>
-        </Main>
-      </DefaultLayout>
-    )
+          {managedChecklists.length ?
+            <>
+              <h2 className="my-16">{capitalize(t('checklists:ongoing_checklists'))}</h2>
+              <p className="mb-16">
+                {t('checklists:you_got_ongoing_checklists', { count: managedChecklists?.length })}
+              </p>
 
+              <FormProvider {...methods}>
+                <OngoingChecklistsTable data={managedChecklists} delegatedChecklists={false} />
+              </FormProvider>
+            </>
+          : null}
+
+          {delegatedChecklists.length ?
+            <div className="py-24">
+              <h2 className="mb-16">{t('common:assigned_introductions')}</h2>
+              <p className="mb-16">
+                {t('checklists:you_got_assigned_introductions', { count: delegatedChecklists?.length })}
+              </p>
+              {delegatedChecklists ?
+                <OngoingChecklistsTable data={delegatedChecklists} delegatedChecklists={true} />
+              : null}
+            </div>
+          : null}
+        </div>
+
+        <FormProvider {...methods}>
+          <DelegateMultipleChecklistsModal checklistIds={checked} onClose={closeHandler} isOpen={isOpen} />
+        </FormProvider>
+      </Main>
+    </DefaultLayout>
   );
 }
 
 export const getServerSideProps = async ({ locale }: { locale: string }) => ({
   props: {
-    ...(await serverSideTranslations(locale, ['common', 'layout', 'crud', 'checklists', 'delegation', 'task', 'templates'])),
-
+    ...(await serverSideTranslations(locale, [
+      'common',
+      'layout',
+      'crud',
+      'checklists',
+      'delegation',
+      'task',
+      'templates',
+    ])),
   },
 });
 
