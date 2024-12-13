61c31748f13d58b9c9bf5fefb6fbe4f9318e7d81
Jerker Osatie <jerker.osatie@devize.se>
2024-12-13 15:18:43 +0100

    Feat: filter orgtree

--- frontend/src/components/admin/admin-sidebar/admin-sidebar.component.tsx
+++ frontend/src/components/admin/admin-sidebar/admin-sidebar.component.tsx
@@ -1,15 +1,27 @@
 import { OrganizationMenu } from '@components/admin/organization-menu/organization-menu.component';
+import { SearchField } from '@sk-web-gui/react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { capitalize } from 'underscore.string';
 
 export default function AdminSidebar() {
   const { t } = useTranslation();
+  const [value, setValue] = useState<string>('');
 
   return (
     <div className="w-[45rem] min-h-full bg-background-content shadow-100 pt-32 pb-24 px-20 flex flex-col gap-40">
       <nav className="flex flex-col gap-16">
         <h1 className="text-label-medium m-0">{capitalize(t('templates:name_other'))}</h1>
-        <OrganizationMenu />
+
+        <SearchField
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          onReset={() => setValue('')}
+          showSearchButton={false}
+          placeholder={t('admin:search_orgtree')}
+        />
+
+        <OrganizationMenu searchValue={value} />
       </nav>
     </div>
   );
