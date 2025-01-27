f626be968f140cb03ce2a7d92744206ae3849c79
Henrik Sandström <henrik.sandstrom@sundsvall.se>
2025-01-27 12:42:12 +0100

    Fix types and remove unused intreface

--- frontend/src/interfaces/permissions.ts
+++
@@ -1,5 +0,0 @@
-export interface Permissions {
-  canEditAdmin: boolean;
-  canViewAdmin: boolean;
-  isManager: boolean;
-}
--- frontend/src/services/user-service/defaults.ts
+++ frontend/src/services/user-service/defaults.ts
@@ -1,10 +1,12 @@
 import { User } from '@data-contracts/backend/data-contracts';
 import { ApiResponse } from '@services/api-service';
-import { Permissions } from '@interfaces/permissions';
+import { Permissions } from '@data-contracts/backend/data-contracts';
 
 export const defaultPermissions: Permissions = {
   canEditAdmin: false,
   canViewAdmin: false,
+  canEditDepartment: false,
+  canViewDepartment: false,
   isManager: false,
 };
 
@@ -15,6 +17,8 @@ export const emptyUser: User = {
   username: '',
   role: 'user',
   permissions: defaultPermissions,
+  organizationId: -1,
+  children: [],
 };
 
 export const emptyUserResponse: ApiResponse<User> = {
