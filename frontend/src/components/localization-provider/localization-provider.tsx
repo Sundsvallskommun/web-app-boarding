4ad7d0f59b934e9f79e24c4f8b47f309d986c674
aliceringstromsk <alice.ringstrom@sundsvall.se>
2026-08-24 13:06:19 +0200

    chore: scaffold app router i18n stack

---
+++ frontend/src/components/localization-provider/localization-provider.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import initLocalization from '@app/i18n';
+import { createInstance, Resource } from 'i18next';
+import { memo, ReactNode } from 'react';
+import { I18nextProvider } from 'react-i18next';
+
+interface LocalizationProviderProps {
+  children: ReactNode;
+  locale: string;
+  namespaces: string[];
+  resources: Resource;
+}
+
+const LocalizationProvider = memo<LocalizationProviderProps>(({ children, locale, namespaces, resources }) => {
+  const i18n = createInstance();
+
+  initLocalization(locale, namespaces, i18n, resources).catch((error: unknown) => {
+    console.error('Failed to initialize localization.', error);
+  });
+
+  return <I18nextProvider {...{ i18n }}>{children}</I18nextProvider>;
+});
+
+LocalizationProvider.displayName = 'LocalizationProvider';
+export default LocalizationProvider;
